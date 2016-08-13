849343b946e25f457b500a5bf6e88df1d3360587
carloswimmer <coiote777@gmail.com>
2016-08-13 19:05:57 -0300

    Inserido o form de escolha de submapa na view 'Welcome', pra isso foi modificado o routes e o AplicationController.

--- app/Http/Controllers/AplicationController.php
+++ app/Http/Controllers/AplicationController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controler;
 use App\Http\Requests;
+use App\Submap;
 
 class AplicationController extends Controller
 {
     
 	// Show initial page
 	public function index() {
-		return view('welcome');	
+		return view('welcome', [
+			'submaps' => Submap::orderBy('name', 'asc')->get()	
+		]);	
 	}
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,40 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bem vindo</div>
+                <div class="panel-heading">
+					Bem vindo, escolha um submapa para ser gerado
+				</div>
 
                 <div class="panel-body">
-                    Depois será o form de inserção de link.
+                    <!-- Display Validation Errors -->
+					@include('common.errors')
+
+					<!-- Generator Form -->
+					<form action="{{ url('generator') }}" method="GET" class="form-horizontal">
+
+						<!-- Submap Picker -->
+						<div class="form-group">
+							<label for="submap-picker" class="col-sm-3 control-label">Submapa</label>
+
+							<div class="col-sm-6">
+								<select name="submap" id="first" class="form-control" >
+									<option value="" selected></option>
+									@foreach ($submaps as $submap)
+										<option value="{{ $submap->id }}">{{ $submap->name }}</option>
+									@endforeach
+								</select>
+							</div>
+						</div>
+
+						<!-- Add Host Button -->
+						<div class="form-group">
+							<div class="col-sm-offset-3 col-sm-6">
+								<button type="submit" class="btn btn-default">
+									<i class="fa fa-btn fa-download"></i>Download
+								</button>
+							</div>
+						</div>
+					</form>
                 </div>
             </div>
         </div>
